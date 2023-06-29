8b821e5d1baa3fc955b37278c5db15f0b953fa7a
Timothy Varjan <tim.varjan@gmail.com>
2023-06-29 11:31:10 -0300

    first commit

---
+++ src/main/java/springblog/CodeupSpringBlogApplication.java
@@ -0,0 +1,13 @@
+package springblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeupSpringBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeupSpringBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/springblog/CodeupSpringBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package springblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeupSpringBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
